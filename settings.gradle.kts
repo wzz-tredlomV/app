96e56863ff221e4e56fdc286a8d74a856714927c
wzz-tredlomV <wzzyyds2011@qq.com>
2026-02-15 21:33:57 +0800

    fix sth. wrong

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,13 +10,6 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        // Filament官方仓库
-        maven { 
-            url = uri("https://storage.googleapis.com/filament-android")
-            content { 
-                includeGroup("com.google.android.filament")
-            }
-        }
     }
 }
 rootProject.name = "Model3DViewer"
