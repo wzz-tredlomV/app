8fc23f00e22225e524b485cdde25329414d1f754
wzz-tredlomV <wzzyyds2011@qq.com>
2026-02-15 23:02:09 +0800

    fix sth. wrong

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,7 +60,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     
     // Filament
-    val filamentVersion = "1.50.0"
+    val filamentVersion = "1.56.2"
     implementation("com.google.android.filament:filament-android:$filamentVersion")
     implementation("com.google.android.filament:filament-utils-android:$filamentVersion")
     implementation("com.google.android.filament:gltfio-android:$filamentVersion")
